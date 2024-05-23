9c2407ae3d30a9af187b0bf9383dcf560dd9a634
NextGenOP <60586402+NextGenOP@users.noreply.github.com>
2024-05-23 21:40:01 +0700

    also refactor type

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -2,6 +2,8 @@ import { validateEvent, verifyEvent, nip19 } from "nostr-tools";
 import config from "../config";
 import { decodenpubKeys, decodenpubKeysFromRecord } from "./decodenpubKeys";
 import type { ServerWebSocket } from "bun";
+import type { Event } from "nostr-tools"
+import type { WSData } from "../lib/bunstrtype";
 
 const authorizedKeys = decodenpubKeys(config.authorized_keys);
 const privateKeys = decodenpubKeysFromRecord(config.private_keys);
@@ -9,11 +11,11 @@ const privateKeys = decodenpubKeysFromRecord(config.private_keys);
 /**
  * Authenticates the incoming request.
  * @param {string} authKey - The authentication key.
- * @param {object} data - The data received from the request.
- * @param {ServerWebSocket} ws - The ServerWebSocket object.
+ * @param {Event} data - The data received from the request.
+ * @param {ServerWebSocket<WSData>} ws - The ServerWebSocket object.
  * @returns {boolean} Returns true if authentication succeeds, otherwise false.
  */
-export function auth(authKey: string, data: object, ws: ServerWebSocket) {
+export function auth(authKey: string, data: Event, ws: ServerWebSocket<WSData>) {
   try {
     // Check if authorized_keys, private_keys are empty and noscraper flag is set
     if (!authorizedKeys.length && !Object.keys(privateKeys).length && !config.noscraper)
@@ -36,7 +38,7 @@ export function auth(authKey: string, data: object, ws: ServerWebSocket) {
 
     // Check if relay url matches
     const tags = Object.fromEntries(data.tags);
-    if (!tags.relay?.includes(ws.data.host)) {
+    if (!tags.relay?.includes(ws.data?.host)) {
       throw new Error("Unmatched relay url.");
     }
 
@@ -48,7 +50,7 @@ export function auth(authKey: string, data: object, ws: ServerWebSocket) {
     // Send success message
     ws.send(JSON.stringify(["OK", data.id, true, `Hello ${data.pubkey}`]));
     return true;
-  } catch (error) {
+  } catch (error: any) {
     // Send error message
     ws.send(JSON.stringify(["OK", data.id, false, error.message]));
     return false;
