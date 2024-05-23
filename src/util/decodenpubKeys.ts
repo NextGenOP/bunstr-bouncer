74962e05b3fd6dabeeb204a99abafd4d381bcd09
NextGenOP <60586402+NextGenOP@users.noreply.github.com>
2024-05-23 19:25:33 +0700

    feat: nostr pubkey decoder

--- src/util/decodenpubKeys.ts
+++ src/util/decodenpubKeys.ts
@@ -1,9 +1,21 @@
 import { nip19 } from "nostr-tools";
-
-export function decodenpubKeys(keys: string[]) {
+/**
+ * Decodes nostr public keys from an array of strings
+ * 
+ * @param {string[]} keys - Array of public keys
+ * @returns {string[]} Decoded public keys
+ */
+export function decodenpubKeys(keys: string[]): string[] {
   return keys?.map((key) => key.startsWith("npub") ? (nip19.decode(key).data as string) : key
   );
 }
+/**
+ * Decodes nostr public keys from a record of strings
+ * 
+ * @param {Record<string, string>} dict - Record of public keys
+ * @returns {Record<string, string>} Decoded record of public keys
+ */
+
 export function decodenpubKeysFromRecord(
   dict: Record<string, string>
 ): Record<string, string> {
